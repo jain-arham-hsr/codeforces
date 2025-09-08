a50bba11a657c2e12d2ba312870bb3be7d943201
Arham Jain <jain.arham.hsr@gmail.com>
2025-09-08 16:53:53 +0530

    Contest 2137

---
+++ contest_1363/problem_B.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int run(string s) {
+    for (int i = 0; i < s.length(); i++){
+        
+    }
+}
+
+int main() {
+  int t;
+  cin >> t;
+  for (int i = 0; i < t; i++) {
+    string s;
+    cin >> s;
+    cout << run(s) << "\n";
+  }
+  return 0;
+}
